6ac30eca92ebc949a0226a7d163b6b7ce9e06bd5
Shist <shist.pupust@mail.ru>
2023-04-21 08:49:27 +0300

    Task 3 have been done

--- scripts/1.js
+++ scripts/1.js
@@ -1,5 +1,11 @@
 // Задание 2
 // Запросите у пользователя его имя. Выведите в окошке (с помощью функции alert) фразу: Привет, Николай!
 // (вместо Николай должно показываться имя, которое ввел пользователь)
-const userName = prompt("Enter your name:");
-alert(`Hello, ${userName}!`);
+const userName2 = prompt("Enter your name:");
+alert(`Hello, ${userName2}!`);
+
+// Задание 3
+// Запросите у пользователя число. Затем запросите степень, в которую нужно возвести это число. Выведите в консоль результат.
+const userNum3 = prompt("Enter the number:");
+const userDegree3 = prompt("Enter degree for the number:");
+console.log(userNum3 ** userDegree3);
