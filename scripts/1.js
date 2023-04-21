6eb7548018b76acb9d7a96daaaddc6c29c5af95d
Shist <shist.pupust@mail.ru>
2023-04-21 10:03:40 +0300

    Task 9 have been done

--- scripts/1.js
+++ scripts/1.js
@@ -13,6 +13,8 @@
 // userDegree3 = userDegree3 === null ? 0 : userDegree3;
 // if (!isNaN(userNum3) && !isNaN(userDegree3)) {
 //   console.log(userNum3 ** userDegree3);
+// } else {
+//     console.log("You've entered not nubmers!");
 // }
 
 // // Задание 5
@@ -45,36 +47,55 @@
 //         num6 *= 10;
 //     }
 //     console.log(num6);
+// } else {
+//     console.log("You've entered not a nubmer!");
 // }
 
-// Задание 7
-// Запросите у пользователя число и запишите его в переменную.
-// (Не забывайте, что от пользователя мы всегда получаем строку).
-// Создайте переменную-результат и присвойте ей значение:
-// если введенное пользователем число меньше 5, то результат будет '0', если больше -  '1'. Выведите результат в консоль.
+// // Задание 7
+// // Запросите у пользователя число и запишите его в переменную.
+// // (Не забывайте, что от пользователя мы всегда получаем строку).
+// // Создайте переменную-результат и присвойте ей значение:
+// // если введенное пользователем число меньше 5, то результат будет '0', если больше -  '1'. Выведите результат в консоль.
 // let userNum7 = prompt("Enter the number:", 0);
 // userNum7 = userNum7 === null ? 0 : userNum7;
 // if (!isNaN(userNum7)) {
 //   const result7 = (userNum7 < 5 ? '0' : '1');
 //   console.log(result7);
+// } else {
+//     console.log("You've entered not a nubmer!");
 // }
 
-// Задание 8
-// Попросить пользователя ввести одно число, следом - второе число. Вывести в консоль максимальное из чисел (наибольшее) с текстом: 
-// Большее число: 7 (вместо 7 будет максимальное число из введенных пользователем).
-// * предусмотреть вариант ввода одинаковых чисел
-let firstUserNum8 = prompt("Enter the first number:", 0);
-firstUserNum8 = firstUserNum8 === null ? 0 : firstUserNum8;
-let secondUserNum8 = prompt("Enter the second number:", 0);
-secondUserNum8 = secondUserNum8 === null ? 0 : secondUserNum8;
-if (!isNaN(firstUserNum8) && !isNaN(secondUserNum8)) {
-  if (firstUserNum8 > secondUserNum8) {
-    console.log(`The greater number: ${firstUserNum8}`);
-  } else if (secondUserNum8 > firstUserNum8) {
-    console.log(`The greater number: ${secondUserNum8}`);
-  } else {
-    console.log(`The numbers ${firstUserNum8} and ${secondUserNum8} are equal!`);
-  }
+// // Задание 8
+// // Попросить пользователя ввести одно число, следом - второе число. Вывести в консоль максимальное из чисел (наибольшее) с текстом:
+// // Большее число: 7 (вместо 7 будет максимальное число из введенных пользователем).
+// // * предусмотреть вариант ввода одинаковых чисел
+// let firstUserNum8 = prompt("Enter the first number:", 0);
+// firstUserNum8 = firstUserNum8 === null ? 0 : firstUserNum8;
+// let secondUserNum8 = prompt("Enter the second number:", 0);
+// secondUserNum8 = secondUserNum8 === null ? 0 : secondUserNum8;
+// if (!isNaN(firstUserNum8) && !isNaN(secondUserNum8)) {
+//   if (firstUserNum8 > secondUserNum8) {
+//     console.log(`The greater number: ${firstUserNum8}`);
+//   } else if (secondUserNum8 > firstUserNum8) {
+//     console.log(`The greater number: ${secondUserNum8}`);
+//   } else {
+//     console.log(`The numbers ${firstUserNum8} and ${secondUserNum8} are equal!`);
+//   }
+// } else {
+//     console.log("You've entered not nubmers!");
+// }
+
+// Задание 9
+// Определить, является ли введенное пользователем число num1 кратным введенному числу num2.
+// Обе переменные запрашиваем у пользователя, ответ выводим в консоль.
+let firstNum9 = prompt("Enter the first number:", 0);
+firstNum9 = firstNum9 === null ? 0 : firstNum9;
+let secondNum9 = prompt("Enter the second number:", 0);
+secondNum9 = secondNum9 === null ? 0 : secondNum9;
+if (!isNaN(firstNum9) && !isNaN(secondNum9)) {
+  firstNum9 % secondNum9 === 0
+    ? console.log(`Yes, ${firstNum9} % ${secondNum9} == 0`)
+    : console.log(`No, ${firstNum9} % ${secondNum9} != 0`);
 } else {
-    console.log("You've entered not nubmers!");
+  console.log("You've entered not nubmers!");
 }
