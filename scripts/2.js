f3ef48f9892514da8ea7e3c58594930adede6b33
Shist <shist.pupust@mail.ru>
2023-04-23 01:39:51 +0300

    Task 2.1 have been done

---
+++ scripts/2.js
@@ -0,0 +1,13 @@
+// Задание 1
+// Перепишите код с использованием тернарного оператора
+// let country = 'Sweden';
+// let access;
+// if (country == 'Sweden') {
+// access = true;
+// } else{
+// access = false;
+// }
+console.log("Task 1");
+let country = "Sweden";
+let access = country == "Sweden" ? true : false;
+console.log(access);
