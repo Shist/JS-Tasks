eec70ed1293db550627b162809a2ac4136058020
Shist <shist.pupust@mail.ru>
2023-05-05 11:40:49 +0300

    Task 2 have been done

--- scripts/arr-str-methods.js
+++ scripts/arr-str-methods.js
@@ -4,10 +4,24 @@
 console.log("Task 1");
 
 const ucFirst = (str) => {
-    const newStr = str[0].toUpperCase() + str.slice(1);
-    return newStr;
-}
+  const newStr = str[0].toUpperCase() + str.slice(1);
+  return newStr;
+};
 
 console.log(`ucFirst("apple") = ${ucFirst("apple")}`);
 console.log(`ucFirst("hello world") = ${ucFirst("hello world")}`);
 console.log(`ucFirst("abc123") = ${ucFirst("abc123")}`);
+
+// Задание 2
+// Напишите функцию checkSpam(str), возвращающую true, если str содержит 'badWord' или 'XXX', а иначе false.
+// Функция должна быть нечувствительна к регистру.
+console.log("Task 2");
+
+const checkSpam = (str) =>
+  str.toLowerCase().includes("badword") || str.toLowerCase().includes("xxx")
+    ? true
+    : false;
+
+    console.log(`checkSpam("abcbAdWoRdabc") = ${checkSpam("abcbAdWoRdabc")}`);
+    console.log(`checkSpam("abcxXxabc") = ${checkSpam("abcxXxabc")}`);
+    console.log(`checkSpam("xbadxwordx") = ${checkSpam("xbadxwordx")}`);
