7e987b83883c912125762022ead6182b3e428ca1
Shist <shist.pupust@mail.ru>
2023-05-09 17:07:05 +0300

    Task 2 have been done

--- scripts/bom-dom.js
+++ scripts/bom-dom.js
@@ -15,3 +15,21 @@ userAgent.includes("Win") ? (detectedOS = "Windows") : null;
 console.log(
   `На компьютере с ОС\n${detectedOS}\nс помощью браузера\n${userAgent}\nя открыл страничку\n${location.href}`
 );
+
+// Задание 2
+// В файле html есть разметка:
+// <ul id="list">
+// <li>Джон</li>
+// <li>Пит</li>
+// <li>Джессика</li>
+// <li>Сара</li>
+// </ul>
+// Вывести в консоль каждое из имен (содержимое каждого li).
+// ПОДСКАЗКИ
+// 1) C помощью метода document.querySelectorAll найдите на странице все пункты списка (li) и запишите в переменную. У вас получится html-коллекция (массив).
+// 2) Обойдите коллекцию с помощью forEach и для каждого li вывкдите в консоль текстовое содержимое (innerText)
+console.log("Task 2");
+const lu = document.querySelectorAll("li");
+lu.forEach((item) => {
+  console.log(item.innerText);
+});
