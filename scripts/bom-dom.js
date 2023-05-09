5852abefad4d69339c6d76d4fa0c17570d1a9087
Shist <shist.pupust@mail.ru>
2023-05-09 17:31:44 +0300

    Task 5 have been done

--- scripts/bom-dom.js
+++ scripts/bom-dom.js
@@ -29,16 +29,26 @@ console.log(
 // 1) C помощью метода document.querySelectorAll найдите на странице все пункты списка (li) и запишите в переменную. У вас получится html-коллекция (массив).
 // 2) Обойдите коллекцию с помощью forEach и для каждого li вывкдите в консоль текстовое содержимое (innerText)
 console.log("Task 2");
-const lu = document.querySelectorAll("li");
+const lu = document.querySelectorAll("li.task-2");
 lu.forEach((item) => {
   console.log(item.innerText);
 });
 
 // Задание 4 (задание 3 почему-то отсутствует)
 // Создать с помощью js абзац (тег p). Добавить в него контент. Добавить к нему стили: размер 36px, жирный шрифт. Добавить абзац с текстом на страницу.
+console.log("Task 4");
 const someP = document.createElement('p');
 someP.classList.add('lorem'); // Добавляем класс из подключенной библиотеки с лоремом
 someP.classList.add('15'); // Добавляем класс, который сделает нам 15 слов в параграфе
 someP.style.fontSize = '36px';
 someP.style.fontWeight = 700; // Тоже самое, что 'bold'
 document.body.appendChild(someP);
+
+// Задание 5
+// Написать в html тег заголовка с текстом "Сегодня:" 
+// После заголовка подготовить в html пустой параграф. С помощью js добавить в него сегодняшнюю дату.
+console.log("Task 5");
+const t5p = document.querySelectorAll("p.task-5");
+t5p.forEach((item) => { // На всякий случай делаем это для Всех потенциально найденных p-шек класса .task-5
+    item.innerText = new Date();
+});
